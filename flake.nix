6fb051c3e1dad424a00d5dc80e37812a10088ae2
Brage Ingebrigtsen <brage.ingebrigtsen@hent.no>
2026-02-23 09:39:38 +0100

    feat(nix): add default package

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
     ...
@@ -14,6 +15,15 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
     in {
+      packages.default = pkgs.buildGoModule {
+        pname = "proof";
+        version = "0.1.0";
+
+        src = self;
+
+        vendorHash = "sha256-eXa3+wiLrShg8kBv3ZxYxqMfnj2wrgg7qRW3pRd2apY=";
+      };
+
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           go
